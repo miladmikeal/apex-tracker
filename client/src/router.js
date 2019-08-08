e170b5695f91964253118086b65fc764859a72f6
Milad Mikeal <miladmikeal@gmail.com>
2019-08-07 19:48:13 -0700

    Finish client side

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Search from './components/Search.vue'
+import Profile from './components/Profile.vue'
 
 Vue.use(Router)
 
@@ -10,16 +11,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'search',
+      component: Search
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/profile/:platform/:gamertag',
+      name: 'profile',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Profile
     }
   ]
 })
